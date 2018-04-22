0ac77cfc601755dfd18bef9e904567239a9ae9ab
Maycow Antunes <maycowantunes@gmail.com>
2018-04-22 00:26:55 -0300

    Hood\Treasure\Rogue query builders.

--- hood/Hood/Exceptions/NoRouteException.php
+++ hood/Hood/Exceptions/NoRouteException.php
@@ -1,13 +1,19 @@
 <?php
-
+/**
+ * 2018 Hood Framework
+ */
 namespace Hood\Exceptions;
-
+/**
+ * Routing Exception
+ * @package Hood\Exceptions
+ * @author Maycow Alexandre Antunes <maycow@maycow.com.br>
+ */
 class NoRouteException extends \Exception
 {
-	/**
-	 * Exception Message
-	 * @see https://github.com/Philipp15b/php-i18n
-	 * @var string
-	 */
-	protected $message = 'No Route for the specified path';
+    /**
+     * Exception Message
+     * @see https://github.com/Philipp15b/php-i18n
+     * @var string
+     */
+    protected $message = 'No Route for the specified path';
 }
---
+++ hood/Hood/Exceptions/QueryBuilderException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 2018 Hood Framework
+ */
+namespace Hood\Exceptions;
+/**
+ * Exception for Rogue QueryBuilder
+ * @package Hood\Exceptions
+ * @author Maycow Alexandre Antunes <maycow@maycow.com.br>
+ */
+class QueryBuilderException extends NyuException{}
